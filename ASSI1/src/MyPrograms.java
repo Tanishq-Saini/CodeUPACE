955d7bb60cc8bce9b9e0772df231f4b64bb50074
Tanishq Saini <167120551+Tanishq-Saini@users.noreply.github.com>
2025-09-24 14:30:30 +0530

    Update MyPrograms.java

--- ASSI1/src/MyPrograms.java
+++ ASSI1/src/MyPrograms.java
@@ -24,7 +24,7 @@ public class MyPrograms {
             case 2:
                 System.out.print("Enter String (like a3b2c4): ");
                 String s2 = userInput.nextLine();
-                System.out.println("Expanded: " + expandString(s2));
+                expandString(s2);
                 break;
 
             case 3:
@@ -36,7 +36,7 @@ public class MyPrograms {
             case 4:
                 System.out.print("Enter String: ");
                 String s3 = userInput.nextLine();
-                System.out.println("The length of Substring is : "+lengthOfSubstring(s3));
+                System.out.println("The length of Substring is : " + lengthOfSubstring(s3));
                 break;
 
             case 5:
@@ -51,88 +51,99 @@ public class MyPrograms {
     }
 
     static void compressString(String str) {
-        int count = 1;
-        for(int i=0; i<str.length() - 1; i++) {
-            if(str.charAt(i) == str.charAt(i + 1)) {
-                count++;
-            } else {
-                System.out.print(str.charAt(i));
-                if(count >= 1) {
-                    System.out.print(count);
-                }
-                count = 1; //reset
+        int[] freq = new int[256]; // to store frequency of ASCII characters
+
+        // count frequency
+        for (int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)]++;
+        }
+
+        // print in order of first appearance
+        boolean[] printed = new boolean[256];
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (!printed[ch]) {
+                System.out.print(ch);
+                System.out.print(freq[ch]);
+                printed[ch] = true;
             }
         }
-        //handle last character
-        System.out.print(str.charAt(str.length() - 1));
-        if (count >= 1) {
-            System.out.print(count);
+    }
+    static void expandString(String expandStr) {
+        try {
+            String result = "";
+            int i = 0;
+
+            while (i < expandStr.length()) {
+                char ch = expandStr.charAt(i++);
+                int count = 0;
+                while (i < expandStr.length() && expandStr.charAt(i) >= '0' && expandStr.charAt(i) <= '9') {
+                    count = count * 10 + (expandStr.charAt(i++) - '0');
+                }
+                if (count == 0) count = 1;
+
+                int j = 0;
+                while (j < count) {
+                    result = result + ch;
+                    j++;
+                }
+            }
+
+            System.out.println("Expanded String: " + result);
+        } catch (Exception e) {
+            System.out.println("Error in Expand: " + e.getMessage());
         }
     }
 
-    static String expandString(String s) {
-        String result = "";
-        for (int i = 0; i < s.length(); i += 2) {
-            char ch = s.charAt(i);
-            int count = s.charAt(i+1) - '0'; //char convert into int
-            for(int j=0; j< count; j++) {
-                result = result + ch;
-            }
-        }
-        return result;
-    }
     static String integerToEnglish(int n) {
-        String[] belowTen = {"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"};
-        String[] belowTwenty = {"","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
-        String[] belowHundred = {"","Ten","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
-        if(n == 0){
+        String[] belowTen = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+        String[] belowTwenty = {"", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        String[] belowHundred = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+        if (n == 0) {
             return "Zero";
         }
-        if(n < 10){
+        if (n < 10) {
             return belowTen[n];
         }
-        if(n < 20 && n != 10){
+        if (n < 20 && n != 10) {
             return belowTwenty[n - 10];
         }
-        if(n < 100){
-            return belowHundred[n / 10] +  (n % 10 != 0 ? " " + belowTen[n % 10]:"");
+        if (n < 100) {
+            return belowHundred[n / 10] + (n % 10 != 0 ? " " + belowTen[n % 10] : "");
         }
-        if(n < 1000){
-            return belowTen[n / 100] + " Hundred" + (n % 100 != 0 ? " " + integerToEnglish(n % 100): "");
+        if (n < 1000) {
+            return belowTen[n / 100] + " Hundred" + (n % 100 != 0 ? " " + integerToEnglish(n % 100) : "");
         }
         return integerToEnglish(n / 1000) + " Thousand" + (n % 1000 != 0 ? " " + integerToEnglish(n % 1000) : "");
     }
 
-    static int lengthOfSubstring(String input){
+    static int lengthOfSubstring(String input) {
         boolean[] visitChar = new boolean[256];
         int start = 0;
         int end = 0;
         int maxLength = 0;
-
-        while(end < input.length()){
+        while (end < input.length()) {
             char currentChar = input.charAt(end);
 
-            if(!visitChar[currentChar]){
+            if (!visitChar[currentChar]) {
                 visitChar[currentChar] = true;
                 end++;
-                maxLength = (maxLength > end-start) ? maxLength : end-start;
+                maxLength = (maxLength > end - start) ? maxLength : end - start;
             } else {
                 visitChar[input.charAt(start)] = false;
                 start++;
             }
         }
-
         return maxLength;
-
     }
 
     static boolean isPrime(int n) {
-        if(n <= 1){
+        if (n <= 1) {
             return false;
         }
         int c = 2;
-        while(c * c <= n){
-            if(n % c == 0){
+        while (c * c <= n) {
+            if (n % c == 0) {
                 return false;
             }
             c++;
